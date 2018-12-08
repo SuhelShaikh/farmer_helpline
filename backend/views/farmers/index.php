46e13ef83add3d40285bbc890bb74dbee44a530d
Suhel Shabir Shaikh <suhailshaikh14@gmail.com>
2018-12-08 15:41:27 +0530

    changes for depdrop down

--- backend/views/farmers/index.php
+++ backend/views/farmers/index.php
@@ -1,9 +1,18 @@
 <?php
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use backend\models\User;
+use kartik\depdrop\DepDrop;
+use kartik\select2\Select2;
+use backend\models\State;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Url;
 $this->title = 'Manage Users';
+$states=ArrayHelper::map(State::find()->orderBy('name')->all(), 'state_id', 'name');
+$executives=ArrayHelper::map(User::find()->orderBy('username')->all(), 'id', 'username');
 
 ?>
+<?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data'],'action'=>'index.php?r=farmers/farmdetails']); ?>
 <!-- Main content -->
   <section class="content-header">
       <h1>
@@ -16,6 +25,83 @@ $this->title = 'Manage Users';
       </ol>
   </section>
   <hr />
+  <div class="row">
+            <div class="col-sm-2">
+               <b>State: </b>
+            </div>
+            <div class="col-sm-4">
+              <?php
+          echo $form->field($model, 'state')->dropDownList($states, ['prompt' => 'Select State', 'id' => 'state-id'])->label(false);
+          ?>
+            </div>
+            <div class="col-sm-2">
+               <b>District: </b>
+            </div>
+            <div class="col-sm-4">
+              <?php
+         
+         echo $form->field($model, 'district')->label(false)->widget(DepDrop::classname(), [
+            'options' => ['id' => 'district-id'],
+            'pluginOptions' => [
+                'depends' => ['state-id'],
+                'placeholder' => 'Select...',
+                'url' => Url::to(['/site/district'])
+            ]
+              ]);
+          ?>
+            </div>
+        </div> 
+        <div class="row">
+            <div class="col-sm-2">
+               <b>Tehsil: </b>
+            </div>
+            <div class="col-sm-4">
+              <?php
+           /*echo $form->field($model, 'mandal')->label(false)->widget(DepDrop::classname(), [
+        'options' => ['id' => 'mandal-id'],
+        'pluginOptions' => [
+            'depends' => ['district-id'],
+            'placeholder' => 'Select...',
+            'url' => Url::to(['/site/mandal'])
+        ]
+    ]);*/
+          ?>
+            </div>
+            <div class="col-sm-2">
+               <b>Village: </b>
+            </div>
+            <div class="col-sm-4">
+              <?php
+        echo $form->field($model, 'village')->label(false)->widget(DepDrop::classname(), [
+        'options' => ['id' => 'village-id'],
+        'pluginOptions' => [
+            'depends' => ['mandal-id'],
+            'placeholder' => 'Select...',
+            'url' => Url::to(['/site/village'])
+        ]
+    ]);
+          ?>
+            </div>
+        </div> 
+        <div class="row">
+        <div class="col-md-2">
+          <label>Select Executive: </label>
+        </div>
+        <div class="col-md-4">
+          <?php
+            echo $form->field($model, 'executive_id')->widget(Select2::classname(), [
+                 'data' => $executives,
+                  'options' =>['placeholder' => 'Select Executive'],
+                  'pluginOptions' => [
+                      'allowClear' => true,
+                  ],
+            ])->label(false);
+          ?>
+        </div>
+        <div class="col-md-6">
+          <?php echo Html::submitButton("Search",['class'=>'btn btn-primary btn-flat','id'=>'btnSubmit']); ?>
+        </div>
+      </div>
   <section class="content">
       <?php if (Yii::$app->session->hasFlash('insert')): ?>
         <div class="row">
